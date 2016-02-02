4c1c9e3fc60faf5dc28abc5701b2cb7d71bbdb5e
Connor Bode <connor.bode@gmail.com>
2016-02-02 17:51:48 -0500

    method for listing zone identifiers

--- cli.py
+++ cli.py
@@ -2,16 +2,38 @@ from lib import Cloudflare, IPInfo, Settings
 from optparse import OptionParser
 
 
-parser = OptionParser(usage="usage: %prog [options] settings_file")
+parser = OptionParser(usage="usage: %prog [options]")
+parser.add_option("-s", "--settings",
+                  dest="settings", help="path to settings file")
+parser.add_option("-a", "--action", dest="action",
+                  help="action to perform: list_zones, update_records")
 
 (options, args) = parser.parse_args()
 
-# verify positional arguments
-if len(args) < 1:
-    parser.error("wrong number of arguments")
+if not options.settings:
+    parser.error("settings file must be specified")
 
-settings_file = args[0]
+if not options.action:
+    parser.error("action must be specified")
 
 settings = Settings.Settings(options.settings)
 cf = Cloudflare.Cloudflare(settings.get_email(), settings.get_api_key())
-ipinfo = IPInfo.IPInfo()
+
+
+def list_zones():
+    cf.load_zones()
+    cf.list_zone_identifiers()
+
+
+def update_records():
+    pass
+
+actions = {
+    "list_zones": list_zones,
+    "update_records": update_records
+}
+
+if options.action not in actions:
+    parser.error("action not recognized")
+
+actions[options.action]()
--- lib/Cloudflare.py
+++ lib/Cloudflare.py
@@ -1,6 +1,10 @@
 import requests
 
 
+class CloudflareException(Exception):
+    pass
+
+
 class Cloudflare:
 
     base_url = 'https://api.cloudflare.com/client/v4'
@@ -14,7 +18,20 @@ class Cloudflare:
 
     def get(self, path):
         url = "{0}{1}".format(Cloudflare.base_url, path)
-        return requests.get(url, headers=self.headers)
+        res = requests.get(url, headers=self.headers)
+        if res.status_code != 200:
+            raise CloudflareException
+        return res
 
-    def get_zones(self):
-        return self.get('/zones')
+    def load_zones(self):
+        res = self.get('/zones')
+        json = res.json()
+        results = json['result']
+        self.zones = {}
+        for result in results:
+            name = result['name']
+            self.zones[name] = result
+
+    def list_zone_identifiers(self):
+        for name, zone in self.zones.iteritems():
+            print("{}: {}".format(name, zone['id']))
