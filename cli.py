92389557d601d98983f0493b7953c2a8a6066854
Connor Bode <connor.bode@gmail.com>
2016-02-02 18:03:22 -0500

    added method to list records

--- cli.py
+++ cli.py
@@ -2,11 +2,13 @@ from lib import Cloudflare, IPInfo, Settings
 from optparse import OptionParser
 
 
-parser = OptionParser(usage="usage: %prog [options]")
+parser = OptionParser()
 parser.add_option("-s", "--settings",
                   dest="settings", help="path to settings file")
 parser.add_option("-a", "--action", dest="action",
-                  help="action to perform: list_zones, update_records")
+                  help="action to perform: list_zones, list_dns_records, update_records")
+parser.add_option("-z", "--zone", dest="zone",
+                  help="zone id (use action 'list_zones' to find)")
 
 (options, args) = parser.parse_args()
 
@@ -25,11 +27,18 @@ def list_zones():
     cf.list_zone_identifiers()
 
 
+def list_dns_records():
+    if not options.zone:
+        parser.error("zone must be specified")
+    cf.print_dns_records(options.zone)
+
+
 def update_records():
     pass
 
 actions = {
     "list_zones": list_zones,
+    "list_dns_records": list_dns_records,
     "update_records": update_records
 }
 
--- lib/Cloudflare.py
+++ lib/Cloudflare.py
@@ -21,12 +21,10 @@ class Cloudflare:
         res = requests.get(url, headers=self.headers)
         if res.status_code != 200:
             raise CloudflareException
-        return res
+        return res.json()['result']
 
     def load_zones(self):
-        res = self.get('/zones')
-        json = res.json()
-        results = json['result']
+        results = self.get('/zones')
         self.zones = {}
         for result in results:
             name = result['name']
@@ -35,3 +33,14 @@ class Cloudflare:
     def list_zone_identifiers(self):
         for name, zone in self.zones.iteritems():
             print("{}: {}".format(name, zone['id']))
+
+    def print_dns_records(self, zone):
+        url = '/zones/{}/dns_records'.format(zone)
+        records = self.get(url)
+        for record in records:
+            dns_type = record['type']
+            name = record['name']
+            ip = record['content']
+            dns_id = record['id']
+            line = "{} {} -> {} ({})".format(dns_type, name, ip, dns_id)
+            print(line)
